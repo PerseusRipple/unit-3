2008e76d77a2ac7cedff9abfcf823dbca4d1c4ea
PerseusRipple <perseus.ripple@gmail.com>
2019-03-20 14:13:39 -0400

    added table chema called SeenAnimals

---
+++ Safari-Adventure/SafariAdventureContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Safari_Adventure
+{
+  public partial class SafariAdventureContext : DbContext
+  {
+    public SafariAdventureContext()
+    {
+    }
+
+    public SafariAdventureContext(DbContextOptions<SafariAdventureContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      if (!optionsBuilder.IsConfigured)
+      {
+        optionsBuilder.UseNpgsql("server=localhost;database=Safari-Adventure");
+      }
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    { }
+  }
+}
---
+++ Safari-Adventure/SeenAnimals.cs
@@ -0,0 +1,18 @@
+namespace Safari_Adventure
+{
+  public class Table
+  {
+    //Id / id (int)
+    public int Id { get; set; }
+
+    //Species / species (string)
+    public string Species { get; set; }
+
+    //CountOfTimesSeen / count_of_times_seen (int)
+    public string CountOfTimesSeen { get; set; }
+
+    //LocationOfLastSeen / location_of_last_seen (string)
+    public string LocationOfLastSeen { get; set; }
+
+  }
+}
