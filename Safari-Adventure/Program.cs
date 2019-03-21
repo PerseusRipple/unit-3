9ff764afc05b1081133007ab57990122bd5ae5fc
PerseusRipple <perseus.ripple@gmail.com>
2019-03-21 07:45:20 -0400

    Added all Animals DB (8 total)

--- Safari-Adventure/Program.cs
+++ Safari-Adventure/Program.cs
@@ -17,8 +17,58 @@ namespace Safari_Adventure
         CountOfTimesSeen = 33,
         LocationOfLastSeen = "Jungle",
       };
+      var Animal2 = new SeenAnimals
+      {
+        Species = "Tiger",
+        CountOfTimesSeen = 24,
+        LocationOfLastSeen = "Jungle",
+      };
+      var Animal3 = new SeenAnimals
+      {
+        Species = "Bear",
+        CountOfTimesSeen = 18,
+        LocationOfLastSeen = "Jungle",
+      };
+      var Animal4 = new SeenAnimals
+      {
+        Species = "Gorilla",
+        CountOfTimesSeen = 47,
+        LocationOfLastSeen = "Jungle",
+      };
+      var Animal5 = new SeenAnimals
+      {
+        Species = "Addax",
+        CountOfTimesSeen = 22,
+        LocationOfLastSeen = "Desert",
+      };
+      var Animal6 = new SeenAnimals
+      {
+        Species = "Dung Beetle",
+        CountOfTimesSeen = 12,
+        LocationOfLastSeen = "Desert",
+      };
+      var Animal7 = new SeenAnimals
+      {
+        Species = "Tortoise",
+        CountOfTimesSeen = 2,
+        LocationOfLastSeen = "Desert",
+      };
+      var Animal8 = new SeenAnimals
+      {
+        Species = "Mongoose",
+        CountOfTimesSeen = 8,
+        LocationOfLastSeen = "Desert",
+      };
 
-      db.SeenAnimals.Add(Animal);
+
+      //   db.SeenAnimals.Add(Animal);
+      db.SeenAnimals.Add(Animal2);
+      db.SeenAnimals.Add(Animal3);
+      db.SeenAnimals.Add(Animal4);
+      db.SeenAnimals.Add(Animal5);
+      db.SeenAnimals.Add(Animal6);
+      db.SeenAnimals.Add(Animal7);
+      db.SeenAnimals.Add(Animal8);
       db.SaveChanges();
 
       //create (insert)
