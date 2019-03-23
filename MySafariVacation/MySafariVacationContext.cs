1a55af4c35c20fb91f112c79cf20ae4149ac101a
PerseusRipple <perseus.ripple@gmail.com>
2019-03-23 14:46:15 -0400

    added packages and created database

---
+++ MySafariVacation/MySafariVacationContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace MySafariVacation
+{
+    public partial class MySafariVacationContext : DbContext
+    {
+        public MySafariVacationContext()
+        {
+        }
+
+        public MySafariVacationContext(DbContextOptions<MySafariVacationContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseNpgsql("server=localhost;database=MySafariVacation");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {}
+    }
+}
