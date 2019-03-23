316fc5eaca2cc9fbd238866f4045f1c1dccf1fe2
PerseusRipple <perseus.ripple@gmail.com>
2019-03-23 15:44:28 -0400

    added all animals to table

--- MySafariVacation/MySafariVacationContext.cs
+++ MySafariVacation/MySafariVacationContext.cs
@@ -4,27 +4,29 @@ using Microsoft.EntityFrameworkCore.Metadata;
 
 namespace MySafariVacation
 {
-    public partial class MySafariVacationContext : DbContext
+  public partial class MySafariVacationContext : DbContext
+  {
+    public MySafariVacationContext()
     {
-        public MySafariVacationContext()
-        {
-        }
-
-        public MySafariVacationContext(DbContextOptions<MySafariVacationContext> options)
-            : base(options)
-        {
-        }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseNpgsql("server=localhost;database=MySafariVacation");
-            }
-        }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {}
     }
+
+    public MySafariVacationContext(DbContextOptions<MySafariVacationContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      if (!optionsBuilder.IsConfigured)
+      {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+        optionsBuilder.UseNpgsql("server=localhost;database=MySafariVacation");
+      }
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    { }
+    //public DbSet<SchemaClass> TableName {get;set}
+    public DbSet<SeenAnimals> Animals { get; set; }
+  }
 }
--- MySafariVacation/Program.cs
+++ MySafariVacation/Program.cs
@@ -2,11 +2,71 @@
 
 namespace MySafariVacation
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+      Console.WriteLine("Let's See Some Animals!");
+      //we need to create a new instance of our database context class
+      var db = new SafariAdventureContext();
+
+      var Animal = new SeenAnimals
+      {
+        Species = "Lion",
+        CountOfTimesSeen = 33,
+        LocationOfLastSeen = "Jungle",
+      };
+      var Animal2 = new SeenAnimals
+      {
+        Species = "Tiger",
+        CountOfTimesSeen = 24,
+        LocationOfLastSeen = "Jungle",
+      };
+      var Animal3 = new SeenAnimals
+      {
+        Species = "Bear",
+        CountOfTimesSeen = 18,
+        LocationOfLastSeen = "Jungle",
+      };
+      var Animal4 = new SeenAnimals
+      {
+        Species = "Gorilla",
+        CountOfTimesSeen = 47,
+        LocationOfLastSeen = "Jungle",
+      };
+      var Animal5 = new SeenAnimals
+      {
+        Species = "Addax",
+        CountOfTimesSeen = 22,
+        LocationOfLastSeen = "Desert",
+      };
+      var Animal6 = new SeenAnimals
+      {
+        Species = "Dung Beetle",
+        CountOfTimesSeen = 12,
+        LocationOfLastSeen = "Desert",
+      };
+      var Animal7 = new SeenAnimals
+      {
+        Species = "Tortoise",
+        CountOfTimesSeen = 2,
+        LocationOfLastSeen = "Desert",
+      };
+      var Animal8 = new SeenAnimals
+      {
+        Species = "Mongoose",
+        CountOfTimesSeen = 8,
+        LocationOfLastSeen = "Desert",
+      };
+      db.SeenAnimals.Add(Animal);
+      db.SeenAnimals.Add(Animal2);
+      db.SeenAnimals.Add(Animal3);
+      db.SeenAnimals.Add(Animal4);
+      db.SeenAnimals.Add(Animal5);
+      db.SeenAnimals.Add(Animal6);
+      db.SeenAnimals.Add(Animal7);
+      db.SeenAnimals.Add(Animal8);
+      db.SaveChanges();
     }
+  }
 }
