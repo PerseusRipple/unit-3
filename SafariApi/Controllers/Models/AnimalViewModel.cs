8d39b6b418187311819e7a34e876e902b60d9c27
PerseusRipple <perseus.ripple@gmail.com>
2019-03-26 21:46:49 -0400

    added Animal View Model

---
+++ SafariApi/Controllers/Models/AnimalViewModel.cs
@@ -0,0 +1,14 @@
+namespace SafariApi.Controllers.Models
+{
+  public class AnimalViewModel
+  {
+    public int Id { get; set; }
+
+    public string Species { get; set; }
+
+    public int CountOfTimesSeen { get; set; }
+
+    public string LocationOfLastSeen { get; set; }
+
+  }
+}
